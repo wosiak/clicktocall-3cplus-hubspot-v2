ccc2e7dca1346a26e544f7d30865a470656402b8
Eduardo Wosiak <eduu.wosiak@gmail.com>
2025-06-06 12:55:57 -0300

    Allow iframe embedding in HubSpot

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,10 @@ const nextConfig = {
         source: '/(.*)',
         headers: [
           {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://app.hubspot.com',
+          },
+          {
             key: 'Content-Security-Policy',
             value: 'frame-ancestors https://app.hubspot.com;',
           },
