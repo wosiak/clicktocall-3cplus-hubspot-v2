7bbe46ea562d4e710b007503c3169435dfd8dc7f
wosiak <eduu.wosiak@gmail.com>
2025-06-10 16:59:40 -0300

    chore: update page title in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Click-to-Call | 3C Plus',
+  description: 'Criado por: Eduardo Wosiak',
+  generator: 'wosiak',
 }
 
 export default function RootLayout({
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body>{children}</body>
     </html>
   )
